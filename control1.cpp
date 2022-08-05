65b8ea893832ac6d93e4fb4e55a89a2d9ccbff76
~ Ananya ~ <playing230106@gmail.com>
2022-08-05 23:30:35 +0530

    Add files via upload

---
+++ control1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num=100;
+	while(num<=200)
+	{
+		cout<<"Value is : "<<num<<"\n";
+		if(num==150)
+		{
+			break;
+		}
+		num=num+10;
+	}
+	cout<<"Out of the loop";
+	return 0;
+}
